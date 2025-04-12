c9527f259dbe4eb90e63f98483d692bea2f49f3d
Samuel Gomez G <samgomezgu@unal.edu.co>
2025-04-11 21:08:09 -0500

    Wrapped app inside of ClerkProvider for authentication.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,23 @@
+import { ClerkProvider } from '@clerk/clerk-expo';
+import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from 'expo-router';
+import { StyleSheet } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <SafeAreaProvider>
+        <SafeAreaView style={styles.safeContainer}>
+          <Stack screenOptions={{ headerShown: false }} />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ClerkProvider>
+  );
 }
+
+const styles = StyleSheet.create({
+  safeContainer: {
+    flex: 1,
+  },
+});
