dd452d2704875c172ff5174c515742f28bf974be
Samuel Gomez G <samgomezgu@unal.edu.co>
2025-04-11 21:29:18 -0500

    Created styles for RootLayout in `src/styles/RoutLayout.ts`. Now using these styles.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,23 +1,18 @@
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
 import { Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import RootLayoutStyle from '@/styles/RootLayout';
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
       <SafeAreaProvider>
-        <SafeAreaView style={styles.safeContainer}>
+        <SafeAreaView style={RootLayoutStyle.safeContainer}>
           <Stack screenOptions={{ headerShown: false }} />
         </SafeAreaView>
       </SafeAreaProvider>
     </ClerkProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  safeContainer: {
-    flex: 1,
-  },
-});
